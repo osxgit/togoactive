49b637325704af36b2b33736f54cd7a08bcf1d1b
pallavi.thakur <pallavi.thakur@togoparts.com>
2023-03-06 11:04:32 +0530

    added event controller api

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,6 @@ Route::middleware('validateJsonRequest')->group(function () {
 //    Route::post('create_client',[ClientController::class,'create'])->name('client.create');
     Route::post('test',[UserController::class,'test'])->name('user.test');
     Route::post('create_user',[UserController::class,'create'])->name('user.create');
-
 });
 
 Route::post('landingPageView', [EventsController::class, 'renderLandingPageApi'])->name('landingPage');
