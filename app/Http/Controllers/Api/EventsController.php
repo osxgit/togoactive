c75d67be6023df932ca5a4f7412f26ad17e84ed1
pallavi.thakur <pallavi.thakur@togoparts.com>
2023-03-09 09:41:36 +0530

    added api to fetch landing page data

--- app/Http/Controllers/Api/EventsController.php
+++ app/Http/Controllers/Api/EventsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 use Illuminate\Support\Facades\Redis;
 
-use App\Models\User;
+use App\Models\User; 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
