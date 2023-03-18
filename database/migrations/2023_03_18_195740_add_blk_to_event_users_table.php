6aecbc840e3c0653540a9646ddc1bcdd3f5794cb
pallavi.thakur <pallavi.thakur@togoparts.com>
2023-03-19 02:03:29 +0530

    api to store free registration

--- database/migrations/2023_03_18_195740_add_blk_to_event_users_table.php
+++ database/migrations/2023_03_18_195740_add_blk_to_event_users_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('event_users', function (Blueprint $table) {
-            //
+            $table->dropColumn('blk');
         });
     }
 };
