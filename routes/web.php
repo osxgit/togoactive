65bf2a9869f528382748e87d5ff3538fe7a44089
pallavi.thakur <pallavi.thakur@togoparts.com>
2023-03-21 11:34:16 +0530

    added datatable for users

--- routes/web.php
+++ routes/web.php
@@ -113,7 +113,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin/event/{id}/achievements/edit/{achievementId}', [AchievementsController::class, 'get'])->name('admin.events.achievements.edit');
     Route::get('/admin/event/{id}/achievements/create', [AchievementsController::class, 'get'])->name('admin.events.achievements.create');
     Route::post('/admin/event/{id}/achievements/{achievementId}', [AchievementsController::class, 'update'])->name('admin.events.achievements.update');
-    Route::delete('/admin/event/{id}/achievements/{achievementId}', [AchievementsController::class, 'delete'])->name('admin.events.achievements.delete');
+    Route::any('/admin/event/{id}/participants_manager', [EventsController::class, 'participants_manager'])->name('admin.events.participantsManager');
+
 });
     Route::get('/admin/event/{id}/unlayer', [EventsController::class, 'unlayer'])->name('admin.events.unlayer');
 
