bf3a5d643b7fa1f51f29faa367c4b22a233a132d
pallavi.thakur <pallavi.thakur@togoparts.com>
2023-03-22 10:10:07 +0530

    updated api to get payment id for registartion

--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -1126,7 +1126,7 @@ return $data;
 
             public function eventRegistrationPaymentId($eventId, $userId){
                 $user= User::where('tgp_userid', $userId)->first();
-                $payment =Payment::where('user_id', $userId)->where('event_id',$eventId)->where('payment_type','registration')->select('id')->first();
+                $payment =Payment::where('user_id', $user->id)->where('event_id',$eventId)->where('payment_type','registration')->select('id')->first();
                 return $payment;  
             }
 }
