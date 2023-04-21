845ba9a387317aebfffb1c7d9ff263905677f504
Sanjay Khatri <sanjay.khatri@togoparts.com>
2023-04-21 10:52:36 +0530

    added cron time in kernel file

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,12 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        Commands\TogoecoCron::class,
+        Commands\ChallengeNotification::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +21,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('TogoecoCron')->everyTwoHours();
+         $schedule->command('ChallengeNotification')->everyTwoHours();
     }
 
     /**
